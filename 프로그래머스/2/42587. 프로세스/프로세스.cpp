6d01c0010ed9455ce74e969e37d22cd41e100fe1
Seungmin Han <104374372+Seungcode@users.noreply.github.com>
2024-05-14 23:58:42 +0900

    [level 2] Title: 프로세스, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct s 
+{
+    int location;
+    int priority;
+};
+
+int solution(vector<int> priorities, int location) 
+{
+    queue<s> q;
+    for(int i = 0; i < priorities.size(); ++i) { q.push({i, priorities[i]}); }
+    sort(begin(priorities), end(priorities), [](int a, int b) { return a > b; });
+
+    int answer = 0;
+    while(true)
+    {
+        s pr = q.front(); q.pop();
+
+        if(priorities[answer] == pr.priority)
+        {
+            ++answer;
+            if(pr.location == location) { break; }
+            continue;
+        }
+
+        q.push(pr);
+    }
+
+    return answer;
+}
