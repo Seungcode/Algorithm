0435b5a12c0332172d356874086e30dcdcd21db0
Seungmin Han <104374372+Seungcode@users.noreply.github.com>
2024-08-19 18:35:13 +0900

    [Gold IV] Title: 문자열 지옥에 빠진 호석, Time: 12 ms, Memory: 23812 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Gold/20166. 문자열 지옥에 빠진 호석/문자열 지옥에 빠진 호석.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int N, M, K;
+string arr[11];
+map<string, int> answer;
+vector<string> res;
+
+int dx[8] = {1, -1, 0, 0, 1, 1, -1, -1};
+int dy[8] = { 0, 0, 1, -1, -1, 1, -1, 1};
+
+void dfs(string a, int x, int y){
+    if(a.length() >= 6) return;
+    if(answer[a] != 0) answer[a] ++;
+    for(int i = 0; i<8; i++){
+        int nx = (x + dx[i] + N)%N;
+        int ny = (y + dy[i] + M)%M;
+        dfs(a+arr[nx][ny] , nx, ny );
+    }
+}
+
+int main() {
+    cin>>N>>M>>K;
+
+    for(int i = 0; i<N; i++) cin>>arr[i];
+    for(int i = 0; i<K; i++){
+        string a;
+        cin>>a;
+        res.push_back(a);
+        answer[a] ++;
+    }
+
+    string temp = "";
+
+    for(int i = 0; i<N; i++){
+        for(int j = 0; j<M; j++) dfs(temp + arr[i][j], i, j);
+    }
+
+    for(auto i : res) cout<<answer[i] - 1<<"\n";
+}
