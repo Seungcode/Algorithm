46e2f47b586b62bf4506861fd10ca8458f387b3d
Seungmin Han <104374372+Seungcode@users.noreply.github.com>
2024-09-10 14:29:06 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.26 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
@@ -1,64 +1,63 @@
 #include <string>
 #include <map>
-#include <cctype>
 #include <iostream>
 
 using namespace std;
 
 int solution(string str1, string str2) {
     int answer = 0;
+    double mul = 0, sum = 0;
     
-    map<string, int> news1;
-    map<string, int> news2;
+    //합집합과 교집합을 찾기 위함
+    map<string, int> exist;
+    map<string, int> exist2;
     
-    double cnt = 0.0;
-    double cnt_old = 0.0;
-    
-    //집합 만들기
+    //1번 문자열 정리
     for(int i = 0; i<str1.length()-1; i++){
-        string temp = "";
+        //알파벳이 아닌 경우 -> 패스
+        if(tolower(str1[i]) - 'a' > 25 || tolower(str1[i]) - 'a' < 0 || 
+           tolower(str1[i+1]) - 'a' > 25 || tolower(str1[i+1]) - 'a' < 0) 
+            continue;
+        
+        //문자열 만들기 -> 소문자로 통일해서 만들기
+        string temp  = "";
         temp += tolower(str1[i]);
         temp += tolower(str1[i+1]);
-        if(temp[0] - 'a' >= 0 && temp[0] - 'a'<26 && temp[1] - 'a' >= 0 && temp[1] - 'a'<26)
-            news1[temp]++;
+        exist[temp] ++;
     }
     
+    //2번 문자열 정리
     for(int i = 0; i<str2.length()-1; i++){
+        if(tolower(str2[i]) - 'a' > 25 || tolower(str2[i]) - 'a' < 0 || 
+           tolower(str2[i+1]) - 'a' > 25 || tolower(str2[i+1]) - 'a' < 0) 
+            continue;
+        
         string temp = "";
         temp += tolower(str2[i]);
         temp += tolower(str2[i+1]);
-        if(temp[0] - 'a' >= 0 && temp[0] - 'a'<26 && temp[1] - 'a' >= 0 && temp[1] - 'a'<26)
-            news2[temp]++;
+        exist2[temp] ++;
     }
     
-    //교집합 및 합집합 구하기
-    for(int i = 0; i<str1.length()-1; i++){
-        string temp = "";
-        temp += tolower(str1[i]);
-        temp += tolower(str1[i+1]);
-        cnt+=max(news1[temp], news2[temp]);
-        cnt_old+=min(news1[temp], news2[temp]);
-        //해당 처리가 있어야 같은 문자가 여러번 카운팅 되어지지 않음
-        news1[temp] = 0;
-        news2[temp] = 0;
+    //합집합과 교집합 확인
+    for(auto i : exist){
+        //교집합 및 합집합 중 여러개 있는 경우 확인
+        if(exist[i.first] && exist2[i.first]){
+            sum += max(exist[i.first], exist2[i.first]);
+            mul += min(exist[i.first], exist2[i.first]);
+        }
+        else sum += i.second;
     }
     
-    for(int i = 0; i<str2.length()-1; i++){
-        string temp = "";
-        temp += tolower(str2[i]);
-        temp += tolower(str2[i+1]);
-        if(news1[temp]==0)
-            cnt+=news2[temp];
-        news2[temp] = 0;
+    //합집합에 집계되어지지 않은 내용 집계
+    for(auto i : exist2){
+        if(exist[i.first] == 0 && i.second){
+            sum += i.second;
+        }
     }
     
-    //답 구하기
-    if(cnt == 0)
-        answer = 65536;
-    else
-        answer = (int)((cnt_old/cnt) * 65536);
-    
-    //문제유형 : 문자열 + 구현 + 해싱
+    //유사도 계산
+    if(mul == 0 && sum == 0) answer = 65536;    
+    else answer = (mul / sum) * 65536;
     
     return answer;
 }
