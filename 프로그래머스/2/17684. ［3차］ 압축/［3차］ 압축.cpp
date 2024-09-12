2084b0aee3ffdecc7e046745f2234a6af809d3ba
Seungmin Han <104374372+Seungcode@users.noreply.github.com>
2024-09-12 13:35:16 +0900

    [level 2] Title: [3차] 압축, Time: 0.71 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.cpp
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.cpp
@@ -1,40 +1,43 @@
 #include <string>
 #include <vector>
 #include <map>
-
 using namespace std;
+
+map<string, int> arr;
+
+//map 초기화 -> 기본 알파벳 넣어두기
+void initArr(){
+    string now = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for(int i = 0; i<26; i++){
+        arr[to_string(now[i])] = i+1;
+    }
+}
 
 vector<int> solution(string msg) {
     vector<int> answer;
-    map<string, int> arr;
-    for(int i = 0; i<msg.length(); i++){
-        string a = "";
-        a += msg[i];
-        arr[a] = (msg[i] - 'A') + 1;
-    }
     
-    int cnt = 0;
-    string now = "";
-    for(int i = 0; i<msg.length(); i++){
-        now = "";
-        for(int j = i; j<msg.length(); j++){
-            now+=msg[j];
-            if(arr[now]!=0) {
-                i++;
-                continue;
-            }
-            else {
-                arr[now] = 27+cnt;
-                cnt++;
-                i--;
-                now.pop_back();
-                answer.push_back(arr[now]);
-                break;
-            }
+    initArr();
+    
+    //인덱스 값
+    int cnt = 27;
+    
+    for(int i = 0; i<msg.length();){
+        string temp = to_string(msg[i]);
+        
+        //존재하는 가장 긴 문자열 + 1까지
+        while(arr[temp] != 0 && i<msg.length()){
+            i++;
+            temp += msg[i];
         }
+        
+        //새로 저장하고
+        arr[temp] = cnt;
+        cnt++;
+        
+        //맨 뒤에꺼 빼고 배열에 값 넣어주기
+        temp.pop_back();
+        answer.push_back(arr[temp]);
     }
-    answer.push_back(arr[now]);
-    return answer;
     
-    //문제유형 : 구현
+    return answer;
 }
