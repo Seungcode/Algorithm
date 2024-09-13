8eddb246909806583c975bcf010c7951ad54b06a
Seungmin Han <104374372+Seungcode@users.noreply.github.com>
2024-09-13 11:47:49 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.cpp
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.cpp
@@ -1,55 +1,47 @@
 #include <string>
+#include <vector>
+#include <iostream>
 
 using namespace std;
 
 int solution(string dartResult) {
     int answer = 0;
     
-    //이전값과 현재 값을 저장할 변수
-    int before = 0;
-    int now = 0;
+    //점수 저장
+    vector<int> arr;
     
-    //계산
-    for(int i = 0; i<dartResult.length(); i++){
+    for(int i = 0; i<dartResult.size();){
         
-        //숫자가 나왔을 때 -> 이전 숫자가 있을 경우 -> 음수이거나 혹은 계산한 값이 10보다 클 경우 -> before과 now 갱신
-        if(dartResult[i] - '0' >= 0 && dartResult[i] - '0' <= 9){
-            if(now < 0){
-                before = now;
-                now = 0;
-            }
-            now = now*10 + (dartResult[i] - '0');
-            if(now>10){
-                before = now/10;
-                now = now%10;
-            }
+        //스코어 계산
+        int score = dartResult[i] - '0';
+        i++;
+        
+        if(dartResult[i] == '0'){
+            score = 10;
+            i++;
         }
-        //보너스 값 계산
-        else if(dartResult[i] =='S' || dartResult[i] == 'D' || dartResult[i] =='T'){
-            if(dartResult[i] == 'S')
-                now = now;
-            else if(dartResult[i] == 'D')
-                now = now*now;
-            else if(dartResult[i] == 'T')
-                now = now*now*now;
-            answer += now;
-        }
-        //옵션 값 계산
-        else{
-            if(dartResult[i] == '*'){
-                answer = answer-now;
-                now = now*2;
-                answer+= before + now;
+        
+        //보너스 및 옵션 계산
+        while(dartResult[i] - '0' > 9 || dartResult[i] - '0' < 0 && i<dartResult.size()){
+            //보너스 계산
+            if(dartResult[i] == 'S') score = score;
+            else if(dartResult[i] == 'D') score *= score;
+            else if(dartResult[i] == 'T') score = score*score*score;
+            
+            //옵션 계산
+            else if(dartResult[i] == '*'){
+                if(!arr.empty()) arr[arr.size() - 1] = arr[arr.size() - 1]*2;
+                score *= 2;
             }
-            else if(dartResult[i] == '#'){
-                answer = answer - now;
-                now = -1*now;
-                answer += now;
-            }
+            else score *= -1;
+            i++;
         }
+        
+        arr.push_back(score);
     }
     
-    //문제유형 : 구현
+    //총 점 계산
+    for(auto i : arr) answer += i;
     
     return answer;
 }
