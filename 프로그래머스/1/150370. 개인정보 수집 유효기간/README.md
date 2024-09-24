63c1a2cc7902c5f2b07ec953b7629deabfec4a6e
Seungmin Han <104374372+Seungcode@users.noreply.github.com>
2024-09-24 13:40:40 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.07 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
@@ -1,10 +1,10 @@
 # [level 1] 개인정보 수집 유효기간 - 150370 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370#) 
 
 ### 성능 요약
 
-메모리: 4.16 MB, 시간: 0.05 ms
+메모리: 4.14 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 26일 20:46:30
+2024년 09월 24일 13:40:37
 
 ### 문제 설명
 
--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
@@ -1,39 +1,57 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <sstream>
-
 using namespace std;
+
+map<char, int> term;
 
 vector<int> solution(string today, vector<string> terms, vector<string> privacies) {
     vector<int> answer;
     
-    //약관 정리용 map 정의
-    map <char, int> ter;
+    //개인정보 보호 기간 정리
+    for(auto i : terms) 
+        term[i[0]] = stoi(i.substr(2, i.length() - 2));
     
-    //오늘 날짜 정리
-    int t_cnt = stoi(today.substr(0, 4))*12*28 + stoi(today.substr(5, 2))*28 + stoi(today.substr(8, 2));
-    
-    //약관 기간 정리
-    for(auto i : terms){
-        int temp = 0;
-        for(int j = 2; j<i.length(); j++){
-            temp = temp*10+(i[j]-'0');
+    //오늘 일자 정리
+    int n_year = stoi(today.substr(0, 4));
+    int n_month = stoi(today.substr(5, 2));
+    int n_day = stoi(today.substr(8, 2));
+
+    for(int i = 0; i<privacies.size(); i++){
+        //개인정보 보호 기간 확인
+        int year = stoi(privacies[i].substr(0, 4));
+        int month = stoi(privacies[i].substr(5, 2)) + term[privacies[i].back()];
+        int day = stoi(privacies[i].substr(8, 2));
+        
+        //12월을 지날 경우 -> 년도 올려주기
+        if(month > 12){
+            year += month/12;
+            month = month%12;
+            if(month == 0) {
+                year --;
+                month = 12;
+            }
         }
-        ter[i[0]] = temp;
+        
+        //유효기간이 지났는지 확인
+        if(year > n_year) continue;
+        if(year < n_year) {
+            answer.push_back(i+1);
+            continue;
+        }
+        
+        if(month > n_month) continue;
+        if(month < n_month) {
+            answer.push_back(i+1);
+            continue;
+        }
+        
+        if(day > n_day) continue;
+        if(day <= n_day) {
+            answer.push_back(i+1);
+            continue;
+        }
     }
     
-    //날짜를 기준으로 조건에 부합하는지 확인
-    int cnt = 1;
-    for(auto i : privacies){
-        int user_cnt = stoi(i.substr(0, 4))*12*28 + stoi(i.substr(5, 2))*28 + stoi(i.substr(8, 2)) + ter[i[i.length()-1]]*28;
-        
-        if(t_cnt>=user_cnt)
-            answer.push_back(cnt);
-        
-        cnt++;
-        }
-    
-    //시간복잡도 O(N) -> 최대 O(100)
     return answer;
 }
