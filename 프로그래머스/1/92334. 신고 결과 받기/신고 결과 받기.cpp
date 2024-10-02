2d4984813b1c14a6831a860f72d551a512e418e0
Seungmin Han <104374372+Seungcode@users.noreply.github.com>
2024-10-02 13:35:31 +0900

    [level 1] Title: 신고 결과 받기, Time: 263.39 ms, Memory: 27.3 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
@@ -4,51 +4,46 @@
 
 using namespace std;
 
+bool visited[1001][1001] = {false, };
+map<string, int> cnt;
+map<string, int> index;
+
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
-    //인덱스로 접근하기 위해 배열 초기화
     vector<int> answer;
-    answer.resize(id_list.size());
     
-    //배열 0으로 초기화
-    int arr[1001][1001] = {0, };
+    answer.resize(id_list.size(), 0);
     
-    //인덱싱 번호 매길 map과 받은 횟수를 저장할 map 정의
-    map <string, int> index;
-    map <string, int> receive;
-
-    //인덱싱 번호 저장
-    for(int i = 0; i<id_list.size(); i++)
-        index[id_list[i]] = i;
+    for(int i = 0; i<id_list.size(); i++) index[id_list[i]] = i;
     
-    //받은 횟수 정리 -> 만약 그 사람이 여러번 보냈을 경우 -> 제외
+    //신고 현황 관리
     for(auto i : report){
-        int flag = 0;
-        string receiver = "";
-        string sender = "";
-        for(int j = 0; j<i.length(); j++){
-            if(i[j] == ' ')
-                flag = 1;
-            else if(flag == 1)
-                receiver += i[j];
-            else
-                sender += i[j];
+        string send = "";
+        string receive = "";
+        
+        for(auto j : i){
+            if(j == ' ') break;
+            send += j;
         }
-        if(arr[index[receiver]][index[sender]] == 0)
-            receive[receiver] ++;
-        arr[index[receiver]][index[sender]] = 1;
+        
+        for(int j = i.length()-1; j>0; j--){
+            if(i[j] == ' ') break;
+            receive = i[j] + receive;
+        }
+        
+        if(!visited[index[receive]][index[send]]){
+            visited[index[receive]][index[send]] = true;
+            cnt[receive]++;
+        }
     }
-
-    //만일 한 사람이 지정 횟수 이상으로 신고를 받았을 경우 -> 보낸 사람 대상으로 메일 수 증가
-    for(auto i : id_list){
-        if(receive[i]>=k){
-            for(int j = 0; j<id_list.size(); j++){
-                if(arr[index[i]][j])
-                    answer[j]++;
+    
+    //문자 받을 수 계산
+    for(auto i : cnt){
+        if(i.second >= k){
+            for(int j = 0; j < id_list.size(); j++){
+                if(visited[index[i.first]][j]) answer[j] ++;
             }
         }
     }
-    
-    //문제 유형 : 구현
     
     return answer;
 }
