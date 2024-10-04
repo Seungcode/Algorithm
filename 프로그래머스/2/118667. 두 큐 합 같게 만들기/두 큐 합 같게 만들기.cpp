38ffd32be753489e7b99e594a0372aa9160d7e11
Seungmin Han <104374372+Seungcode@users.noreply.github.com>
2024-10-04 17:26:59 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 6.32 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int solution(vector<int> queue1, vector<int> queue2) {
     int answer = 0;
     
+    //pop_front를 사용하기 위해 deque로 이전 & 합 구하기
     long one = 0, two = 0;
     deque<int> arr1;
     deque<int> arr2;
@@ -19,14 +20,18 @@ int solution(vector<int> queue1, vector<int> queue2) {
     }
     
     while(true){
+        //같을 경우
         if(one == two) break;
-        else if(answer > 4*queue1.size()) return -1;
+        //최대 수보다 클 경우 -> 큐에 왔다 갔다하는 최대 경우보다 크면 -1
+        else if(answer > 2*queue1.size() + 1) return -1;
+        //1번큐의 값이 더 클 경우
         else if(one < two){
             arr1.push_back(arr2.front());
             two -= arr2.front();
             one += arr2.front();
             arr2.pop_front();
         }
+        //2번 큐의 값이 더 클경우
         else {
             arr2.push_back(arr1.front());
             two += arr1.front();
