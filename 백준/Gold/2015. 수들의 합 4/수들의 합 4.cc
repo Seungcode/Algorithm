dc765f3588ef03fb53515c4076599db01fbb0f38
Seungmin Han <104374372+Seungcode@users.noreply.github.com>
2025-04-29 15:34:44 +0900

    [Gold IV] Title: 수들의 합 4, Time: 40 ms, Memory: 12316 KB -BaekjoonHub

---
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int N, K;
+int arr[200001] = {0, };
+unordered_map<long long, long long> cnt;
+
+void input() {
+    cin>>N>>K;
+
+    for(int i = 1; i <= N; i++){
+        cin>>arr[i];
+    }
+}
+
+void solution() {
+    input();
+
+    long long answer = 0;
+
+    for(int i = 1; i <= N; i++){
+        arr[i] += arr[i-1];
+        if(arr[i] == K) answer++;
+        answer += cnt[arr[i] - K];
+        cnt[arr[i]]++;
+    }
+
+    cout<<answer;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solution();
+}
