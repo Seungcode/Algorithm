c7bf0997f17a17a8ab7dcf49f309883b346fe9c4
Seungmin Han <104374372+Seungcode@users.noreply.github.com>
2025-05-27 23:51:59 +0900

    [Gold V] Title: 사과나무, Time: 16 ms, Memory: 2376 KB -BaekjoonHub

--- 백준/Gold/20002. 사과나무/사과나무.cc
+++ 백준/Gold/20002. 사과나무/사과나무.cc
@@ -6,28 +6,18 @@ int N;
 int arr[301][301] = {0, };
 int answer = -1000 * 300 * 300;
 
-void input() {
+void solution() {
     cin >> N;
 
     for(int i = 1; i <= N; i++){
-        for(int j = 1; j <= N; j++) cin>>arr[i][j];
-    }
-}
-
-void makeSum(){
-    for(int i = 1; i<=N; i++){
         for(int j = 1; j <= N; j++) {
+            cin>>arr[i][j];
             arr[i][j] += arr[i-1][j] + arr[i][j-1] - arr[i-1][j-1];
             for(int z = 1; z <= min(i, j); z++){
                 answer = max(answer, arr[i][j] - arr[i-z][j] - arr[i][j-z] + arr[i-z][j-z]);
             }
         }
     }
-}
-
-void solution() {
-    input();
-    makeSum();
     cout<<answer;
 }
 
